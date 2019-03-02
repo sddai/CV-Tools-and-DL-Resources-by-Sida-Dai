26f636bd76d34cdf96004dc79a12c9fc4b28fa21
sddai <sida.dai.86@gmail.com>
2019-03-02 16:14:11 +0800

    本文件不要和被重命名文件放在一起

---
+++ data_rename.py
@@ -0,0 +1,24 @@
+# 文件批量重命名
+
+import os
+import sys
+
+path = "../data/"
+
+def rename(path):
+	count = 0
+	ori_dir = path
+	for files in os.listdir(ori_dir):
+		# Pass when a file is a folder #
+		old_dir = os.path.join(ori_dir, files)
+		if os.path.isdir(old_dir):
+			continue
+		# Rename #
+		file_name = os.path.splitext(files)[0]
+		file_type = os.path.splitext(files)[1]
+		new_dir = os.path.join(ori_dir, str(count)+file_type)
+		os.rename(old_dir, new_dir)
+		count += 1
+		print('count:', count)
+rename(path)
+
